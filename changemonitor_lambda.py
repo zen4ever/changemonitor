86816b5353151727fb1b75ec302bfd2e5aa5e948
Andrew Kurinnyi <akurinnyi@whalerockindustries.com>
2015-10-22 11:58:33 -0700

    Added more logging

--- changemonitor_lambda.py
+++ changemonitor_lambda.py
@@ -19,10 +19,15 @@ CHANGEMONITOR_SNS_TOPIC = os.environ.get('CHANGEMONITOR_SNS_TOPIC')
 
 
 def handler(event, context):
+    logger.info("Got event: {event}".format(event))
+    logger.info("Checking your url: {url}".format(url=CHANGEMONITOR_URL))
     diff = check_if_changed(CHANGEMONITOR_URL, CHANGEMONITOR_BUCKET)
     diff_text = "\n".join(diff)
     if diff_text:
+        logger.info("Change detected")
         notify(CHANGEMONITOR_SNS_TOPIC, diff_text, CHANGEMONITOR_URL)
+    else:
+        logger.info("No changes detected")
 
 
 if __name__ == "__main__":
