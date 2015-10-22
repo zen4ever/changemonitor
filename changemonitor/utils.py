78124c3c64e3594d7e8ef97d24316b1da16adaf6
Andrew Kurinnyi <akurinnyi@whalerockindustries.com>
2015-10-22 16:10:39 -0700

    Added some docstrings

--- changemonitor/utils.py
+++ changemonitor/utils.py
@@ -6,6 +6,11 @@ from botocore.exceptions import ClientError
 
 
 def check_if_changed(url, bucket_name):
+    """
+    Polls a given URL, checks if there are any differences from previous version.
+    Then stores a current version in S3 bucket.
+    Returns a unified diff generator.
+    """
     response = requests.get(url)
     s3 = boto3.resource('s3')
     key = hashlib.md5(url).hexdigest()
@@ -24,6 +29,9 @@ def check_if_changed(url, bucket_name):
 
 
 def notify(sns_topic_arn, diff_text, url):
+    """
+    Sends a message with your changes to to SNS topic
+    """
     sns = boto3.resource('sns')
     topic = sns.Topic(sns_topic_arn)
     topic.publish(
